3742d5b61080778cde71f624396e687bca0af0c0
unknown <Brandon Jenkins@Charlotte.in.dynetics.com>
2023-12-08 17:46:51 -0800

    First UI add
    adding super basic main menu with buttons and signals

---
+++ ui/mainmenu/MainMenu.gd
@@ -0,0 +1,9 @@
+extends MarginContainer
+
+
+func _on_Button_pressed(extra_arg_0):
+	if extra_arg_0 == "Continue":
+		get_tree().change_scene_to_file("res://levels/game_level.tscn")
+	elif extra_arg_0 == "ExitGame":
+		get_tree().quit()
+	
