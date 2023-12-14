e937754ecc5080dc7df382f3ce4abc462f4f969c
tristanmelton <30941629+tristanmelton@users.noreply.github.com>
2023-12-13 18:17:11 -0800

    Spawn Enemies and Follow Path (#7)
    * Following a Path base code for enemies.
    
    * Add ease option to change how it eases in and out of speed
    
    * Made path following object inherent, contains path following object rather than attached to another PathFollow2D
    
    * Path now has attached script that spawns sub-scenes, which destroy at the end of the path.
    
    * Tidying up
    
    * Tidying up some file names
    
    * animation test
    
    I don't think I did the collision right
    
    * playing with animation again
    
    * direction based character animation
    
    very basic animation depending on movement direction
    
    ---------
    
    Co-authored-by: Tristan Melton <tristanmelton@engineering.ucla.edu>
    Co-authored-by: Alex-Q-W <152808415+Alex-Q-W@users.noreply.github.com>

---
+++ scripts/spawn_enemies_on_path.gd
@@ -0,0 +1,34 @@
+extends Path2D
+
+@export var path_time : float = 6
+@export var spawn_time : float = 0.5
+var tween
+var timer
+
+const path_follow_obj = preload("res://objects/enemy.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true)
+	
+	timer = Timer.new()
+	add_child(timer)
+	timer.connect("timeout", _on_timer_timeout)
+	timer.set_wait_time(spawn_time)
+	timer.set_one_shot(false)
+	timer.start()
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+func _on_timer_timeout():
+	var instance = path_follow_obj.instantiate()
+	self.add_child(instance)
+	tween = get_tree().create_tween()
+	tween.bind_node(instance)
+	tween.set_trans(Tween.TRANS_LINEAR)
+	tween.set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(instance, "progress_ratio", 1, path_time)
+	tween.set_loops(1)
