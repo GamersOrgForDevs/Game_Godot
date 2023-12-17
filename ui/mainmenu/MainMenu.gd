04eaa37198df659a7021d7fec1b17de13f02438c
tristanmelton <30941629+tristanmelton@users.noreply.github.com>
2023-12-17 10:36:24 -0800

    Day night phase (#13)
    * Add NightTime scene, access it via the NewGame button on the main menu. Uses CanvasModulate to make teverything dark and PointLights2Ds for light, with custom lightmap texture. Had to override the ingame_night UI material to react to light as unshaded so it didn't get included in the CanvasModulate.
    
    * Add Light Occlusion as an example (non-closed). You need to turn on the shadows option in the point light for it to be used in shadow calculations.
    
    * Modification of night level with occlusion

--- ui/mainmenu/MainMenu.gd
+++ ui/mainmenu/MainMenu.gd
@@ -6,3 +6,6 @@ func _on_Button_pressed(extra_arg_0):
 		get_tree().change_scene_to_file("res://levels/game_level.tscn")
 	elif extra_arg_0 == "ExitGame":
 		get_tree().quit()
+	elif extra_arg_0 == "NewGame":
+		get_tree().change_scene_to_file("res://levels/night_level.tscn")
+
