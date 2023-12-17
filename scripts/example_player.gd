fb59addbb7621e68cfb4655d08827da1ed2bdf7d
tristanmelton <30941629+tristanmelton@users.noreply.github.com>
2023-12-17 10:30:03 -0800

    End of path damage (#12)
    * Added end_of_path signal to enemy which passes damage signal to the path. Added do_damage signal to spawn_enemies on the Path2D which passes a damage step to the in-game UI. In-game UI connects to signal, takes decrements health based on passed-value. If health is less than or equal to zero, send to main menu.
    
    * Removed unnecessary print statement, tweaked path to be a little more obvious of what's happening
    
    * Added mana and coins, which you can spend with some keyboard presses and do nothing with. The variables are kept in the player script, I'm looking into signals on variable update rather than having to do it manually each time

--- scripts/example_player.gd
+++ scripts/example_player.gd
@@ -2,6 +2,24 @@ extends CharacterBody2D
 
 @export var moveSpeed : float = 100
 
+signal update_healthbar(health)
+signal update_mana(mana)
+signal update_coins(coins)
+
+var health = 100
+var mana = 100
+var coins = 30
+
+var enemypath = 0
+
+func _ready():
+	enemypath = get_tree().get_root().get_node("GameLevel").get_node("EnemyPath")
+	enemypath.connect("do_damage", _taken_damage)
+
+func _process(_delta):
+	if(health <= 0):
+		get_tree().change_scene_to_file("res://ui/mainmenu/MainMenu.tscn")
+
 func _physics_process(_delta):
 	#Set input direction
 	var inputDirection = Vector2(
@@ -24,5 +42,32 @@ func _physics_process(_delta):
 	else:
 		$AnimatedSprite2D.play("Idle")
 	
+	#Dummy spend money button
+	if Input.is_action_just_pressed("use_mana_standin"):
+		if mana >= 15:
+			mana -= 15
+			update_mana.emit(mana)
+	if Input.is_action_pressed("charge_mana_standin"):
+		mana += 0.5
+		if(mana >= 100):
+			mana = 100
+		elif(mana <= 0):
+			mana = 0
+		update_mana.emit(mana)
+	if Input.is_action_just_pressed("purchase_standin"):
+		coins -= 1
+		if coins <= 0:
+			coins = 0
+		update_coins.emit(coins)
+	if Input.is_action_just_pressed("earn_standin"):
+		coins += 1
+		update_coins.emit(coins)
+	#Dummy spend mana button
+	
 	#Move and SLide function uses velocity of character body to move character on map
 	move_and_slide()
+
+func _taken_damage(damage):
+	health -= damage
+	print("Damage Taken " + str(damage))
+	update_healthbar.emit(health)
--- scripts/ingame_ui.gd
+++ scripts/ingame_ui.gd
@@ -1,12 +1,20 @@
 extends Control
 
-var health = 100
+var health_value = 100
+var mana_value = 100
 var score_value = 0
+
 @onready var score_value_text = %ScoreValueText
 @onready var test_progress = %TestProgress
+@onready var mana_progress = %ManaProgress
 
+var char = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	char = get_tree().get_root().get_node("GameLevel").get_node("ExamplePlayer")
+	char.connect("update_healthbar", _update_healthbar)
+	char.connect("update_mana", _update_mana)
+	char.connect("update_coins", _update_coins)
 	pass # Replace with function body.
 
 
@@ -16,14 +24,21 @@ func _process(delta):
 
 func _on_increase_score_pressed():
 	score_value += 1
-	print(score_value)
 	score_value_text.text = str(score_value)
 
-
-
 func _on_take_damage_pressed():
-	health -= 10
-	if(health < 0):
-		health = 100
-	test_progress.value = health
+	health_value -= 10
+	if(health_value <= 0):
+		health_value = 100
+	test_progress.value = health_value
+	
+func _update_healthbar(new_health):
+	health_value = new_health
+	test_progress.value = health_value
+func _update_mana(new_mana):
+	mana_value = new_mana
+	mana_progress.value = mana_value
+func _update_coins(new_coins):
+	score_value = new_coins
+	score_value_text.text = str(score_value)
 	
